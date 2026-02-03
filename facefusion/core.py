7a65865c89a6b534273ba69ae723b4b809afcab6
tony TT <275116547@qq.com>
2026-02-04 00:58:47 +0800

    更新 core.py
    调试2

--- facefusion/core.py
+++ facefusion/core.py
@@ -118,11 +118,10 @@ def common_pre_check() -> bool:
 		voice_extractor
 	]
 
-	content_analyser_content = inspect.getsource(content_analyser).encode()
-	content_analyser_hash = hash_helper.create_hash(content_analyser_content)
+	# content_analyser_content = inspect.getsource(content_analyser).encode()
+	# content_analyser_hash = hash_helper.create_hash(content_analyser_content)
 
-	return all(module.pre_check() for module in common_modules) and content_analyser_hash == 'b14e7b92'
-
+	return all(module.pre_check() for module in common_modules)  # hash check disabled
 
 def processors_pre_check() -> bool:
 	for processor_module in get_processors_modules(state_manager.get_item('processors')):
